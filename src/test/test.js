f6bb1c75d4bc56931754960d141b868537c9f415
GennadiyUsatenko <usatenko.gena@gmail.com>
2019-03-17 22:27:13 +0200

    Added the well-known Cartesian coordinate system

--- src/test/test.js
+++ src/test/test.js
@@ -3,10 +3,17 @@ function draw() {
     if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
 
+        useCartesianCoordinateSystem(canvas);
         ctx.beginPath();
         ctx.moveTo(75, 50);
         ctx.lineTo(100, 75);
         ctx.lineTo(100, 25);
         ctx.fill();
     }
+}
+
+function useCartesianCoordinateSystem(canvas) {
+    var ctx = canvas.getContext('2d');
+    ctx.translate(0,canvas.height);
+    ctx.scale(1,-1);
 }
