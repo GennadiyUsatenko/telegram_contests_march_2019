75aa90c0e45a682b2c9af9459e5250f5cca47ccb
GennadiyUsatenko <usatenko.gena@gmail.com>
2019-03-18 23:27:48 +0200

    Added the loadJsonData function to test.js file

--- src/test/test.js
+++ src/test/test.js
@@ -7,13 +7,22 @@ function draw() {
         ctx.beginPath();
         ctx.moveTo(75, 50);
         ctx.lineTo(100, 75);
-        ctx.lineTo(100, 25);
-        ctx.fill();
+        ctx.lineTo(200, 75);
+        ctx.lineTo(400, 55);
+        ctx.lineTo(800, 75);
+        ctx.lineTo(1800, 55);
+        ctx.stroke();
     }
 }
 
 function useCartesianCoordinateSystem(canvas) {
     var ctx = canvas.getContext('2d');
-    ctx.translate(0,canvas.height);
-    ctx.scale(1,-1);
+    ctx.translate(0, canvas.height);
+    ctx.scale(1, -1);
+}
+
+function loadJsonData() {
+    return fetch('/telegram_contests/src/resources/chart_data.json').then(function(response) {
+        return response.json();
+    });
 }
