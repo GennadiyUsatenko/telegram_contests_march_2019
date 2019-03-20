80c20ff64e2dd50efda22053645e8484d8ecb3c9
GennadiyUsatenko <usatenko.gena@gmail.com>
2019-03-20 23:56:40 +0200

    Added the animation fail

--- src/test/test.js
+++ src/test/test.js
@@ -1,14 +1,50 @@
+var arr = [{x:25,y:100},{x:50,y:25}];
+
+function animation(canvas) {
+    if (arr.length > 8) return;
+    var minY = 25;
+    var maxY = 100;
+    var step = 25;
+    var limit = arr.length;
+
+    for(var i = 1; i <= limit; i++) {
+        var x = i * step + limit * step;
+        if(i % 2 == 0) {
+            arr.push({x:x, y:minY});
+        } else {
+            arr.push({x:x, y:maxY});
+        }
+    }
+
+    var ctx = canvas.getContext('2d');
+    ctx.strokeStyle = '#D40000';
+    ctx.lineWidth = 6;
+    //ctx.save();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //ctx.scale(4 / arr.length, 1);
+    ctx.beginPath();
+
+    arr.forEach(function(item){
+        ctx.lineTo(item.x, item.y);
+    });
+
+    ctx.stroke();
+    //ctx.restore();
+    setTimeout(function() {animation(canvas)}, 2000);
+}
+
 function draw() {
     var canvas = document.getElementById('top-chart');
     if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, 300, 300);
         ctx.strokeStyle = '#D40000';
         ctx.lineWidth = 6;
 
         useCartesianCoordinateSystem(canvas);
         ctx.beginPath();
-        ctx.moveTo(75, 50);
-        ctx.lineTo(100, 75);
+        //ctx.moveTo(75, 50);
+        ctx.lineTo(0, 75);
         ctx.lineTo(125, 25);
         ctx.lineTo(150, 100);
         ctx.lineTo(175, 50);
@@ -19,8 +55,7 @@ function draw() {
         ctx.lineTo(325, 125);
         ctx.stroke();
 
-        setTimeout(function() {var ctx = canvas.getContext('2d');ctx.clearRect(0, 0, 300, 300);}, 1000);
-        setTimeout(function() {draw()}, 2000);
+        setTimeout(function() {animation(canvas)}, 2000);
     }
 }
 
